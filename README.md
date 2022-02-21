0e161526bc7b02159e77b5f186c206affa3d141f
tamir-michaeli <tamir.michaeli@logz.io>
2022-02-21 12:04:47 +0200

    x509 certificate metrics lambda

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-init repo
+# X509 Certificate Lambda
+A lambda designed to collect X509 certificate metrics from a URL.
+The collected metrics will be exported to the Logzio app.
+
+
+#### Full list of configurable environment variables
+
+| Environment variable                         | Description                                                                                                                                                                                              |
+|----------------------------------------------|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| LogzioToken (Required)                       | Token for shipping metrics to your Logz.io account. Find it under Settings > Manage accounts. [_How do I look up my Metrics account token?_](/user-guide/accounts/finding-your-metrics-account-token/)   |
+| LogzioListener (Required)                    | Your logzio listener url (for metrics).                                                                                                                                                                  |
+| LogLevel (Optional)                          | Lambda log level. Default: ERROR                                                                                                                                                                         |
+ | LambdaName (Optional)                        | Name for the current lambda function. Default: x509-Certificate-Metrics-Lambda                                                                                                                           |
+| CertificateURL (Required)                    | The URL to collect x509 certificate metrics from, including port. i.e: https://app.logz.io:443                                                                                                           |
+| LambdaTimeout (Optional)                     | The amount of time that Lambda allows a function to run before stopping it. Minimum value is 1 second and Maximum value is 900 seconds. We recommend to start with 300 seconds (5 minutes). Default: 300 |
+| CloudWatchEventScheduleExpression (Optional) | The scheduling expression that determines when and how often the Lambda function runs. We recommend to start with 10 hour rate. Default: rate(10 hours)                                                  |
---
+++ main.go
@@ -0,0 +1,539 @@
+/*
+Code is taken and modified from:
+github.com/influxdata/telegraf
+*/
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/influxdata/telegraf/config"
+	_tls "github.com/influxdata/telegraf/plugins/common/tls"
+	"github.com/pion/dtls/v2"
+	log "github.com/sirupsen/logrus"
+	"go.opentelemetry.io/contrib/exporters/metric/cortex"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"net"
+	"net/url"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"time"
+)
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+func HandleRequest(ctx context.Context) (string, error) {
+	initLogger()
+	log.Debugln("Checking certificate metrics for URL: " + os.Getenv("CERTIFICATE_URL"))
+	err := run()
+	if err != nil {
+		log.Error("Encountered an error: ", err.Error())
+		return "", err
+	}
+
+	return "Lambda finished", nil
+}
+
+func initLogger() {
+	log.SetOutput(os.Stdout)
+	logLevel := os.Getenv("LOG_LEVEL")
+	switch logLevel {
+	case "INFO":
+		log.SetLevel(log.InfoLevel)
+	case "DEBUG":
+		log.SetLevel(log.DebugLevel)
+	case "WARN":
+		log.SetLevel(log.WarnLevel)
+	case "ERROR":
+		fallthrough
+	default:
+		log.SetLevel(log.ErrorLevel)
+	}
+}
+
+const sampleConfig = `
+  ## List certificate sources
+  ## Prefix your entry with 'file://' if you intend to use relative paths
+  sources = ["tcp://example.org:443", "https://influxdata.com:443",
+            "udp://127.0.0.1:4433", "/etc/ssl/certs/ssl-cert-snakeoil.pem",
+            "/etc/mycerts/*.mydomain.org.pem", "file:///path/to/*.pem"]
+  ## Timeout for SSL connection
+  # timeout = "5s"
+  ## Pass a different name into the TLS request (Server Name Indication)
+  ##   example: server_name = "myhost.example.org"
+  # server_name = ""
+  ## Don't include root or intermediate certificates in output
+  # exclude_root_certs = false
+  ## Optional TLS Config
+  # tls_ca = "/etc/telegraf/ca.pem"
+  # tls_cert = "/etc/telegraf/cert.pem"
+  # tls_key = "/etc/telegraf/key.pem"
+`
+const description = "Reads metrics from a SSL certificate"
+
+// X509Cert holds the configuration of the plugin.
+type X509Cert struct {
+	Sources          []string        `toml:"sources"`
+	Timeout          config.Duration `toml:"timeout"`
+	ServerName       string          `toml:"server_name"`
+	ExcludeRootCerts bool            `toml:"exclude_root_certs"`
+	tlsCfg           *tls.Config
+	_tls.ClientConfig
+	locations []*url.URL
+	globpaths []*GlobPath
+}
+
+type X509CertMetrics struct {
+	Tags   map[string]string
+	Fields map[string]int64
+}
+
+// Description returns description of the plugin.
+func (c *X509Cert) Description() string {
+	return description
+}
+
+// SampleConfig returns configuration sample for the plugin.
+func (c *X509Cert) SampleConfig() string {
+	return sampleConfig
+}
+
+func (c *X509Cert) sourcesToURLs() error {
+	for _, source := range c.Sources {
+		if strings.HasPrefix(source, "file://") ||
+			strings.HasPrefix(source, "/") {
+			source = filepath.ToSlash(strings.TrimPrefix(source, "file://"))
+			g, err := Compile(source)
+			if err != nil {
+				return fmt.Errorf("could not compile glob %v: %v", source, err)
+			}
+			c.globpaths = append(c.globpaths, g)
+		} else {
+			if strings.Index(source, ":\\") == 1 {
+				source = "file://" + filepath.ToSlash(source)
+			}
+			u, err := url.Parse(source)
+			if err != nil {
+				return fmt.Errorf("failed to parse cert location - %s", err.Error())
+			}
+			c.locations = append(c.locations, u)
+		}
+	}
+
+	return nil
+}
+
+func (c *X509Cert) serverName(u *url.URL) (string, error) {
+	if c.tlsCfg.ServerName != "" {
+		if c.ServerName != "" {
+			return "", fmt.Errorf("both server_name (%q) and tls_server_name (%q) are set, but they are mutually exclusive", c.ServerName, c.tlsCfg.ServerName)
+		}
+		return c.tlsCfg.ServerName, nil
+	}
+	if c.ServerName != "" {
+		return c.ServerName, nil
+	}
+	return u.Hostname(), nil
+}
+
+func (c *X509Cert) getCert(u *url.URL, timeout time.Duration) ([]*x509.Certificate, error) {
+	protocol := u.Scheme
+	switch u.Scheme {
+	case "udp", "udp4", "udp6":
+		ipConn, err := net.DialTimeout(u.Scheme, u.Host, timeout)
+		if err != nil {
+			return nil, err
+		}
+		defer ipConn.Close()
+
+		serverName, err := c.serverName(u)
+		if err != nil {
+			return nil, err
+		}
+
+		dtlsCfg := &dtls.Config{
+			InsecureSkipVerify: true,
+			Certificates:       c.tlsCfg.Certificates,
+			RootCAs:            c.tlsCfg.RootCAs,
+			ServerName:         serverName,
+		}
+		conn, err := dtls.Client(ipConn, dtlsCfg)
+		if err != nil {
+			return nil, err
+		}
+		defer conn.Close()
+
+		rawCerts := conn.ConnectionState().PeerCertificates
+		var certs []*x509.Certificate
+		for _, rawCert := range rawCerts {
+			parsed, err := x509.ParseCertificate(rawCert)
+			if err != nil {
+				return nil, err
+			}
+
+			if parsed != nil {
+				certs = append(certs, parsed)
+			}
+		}
+
+		return certs, nil
+	case "https":
+		protocol = "tcp"
+		fallthrough
+	case "tcp", "tcp4", "tcp6":
+		ipConn, err := net.DialTimeout(protocol, u.Host, timeout)
+		if err != nil {
+			return nil, err
+		}
+		defer ipConn.Close()
+
+		serverName, err := c.serverName(u)
+		if err != nil {
+			return nil, err
+		}
+		c.tlsCfg.ServerName = serverName
+
+		c.tlsCfg.InsecureSkipVerify = true
+		conn := tls.Client(ipConn, c.tlsCfg)
+		defer conn.Close()
+
+		// reset SNI between requests
+		defer func() { c.tlsCfg.ServerName = "" }()
+
+		hsErr := conn.Handshake()
+		if hsErr != nil {
+			return nil, hsErr
+		}
+
+		certs := conn.ConnectionState().PeerCertificates
+
+		return certs, nil
+	case "file":
+		content, err := os.ReadFile(u.Path)
+		if err != nil {
+			return nil, err
+		}
+		var certs []*x509.Certificate
+		for {
+			block, rest := pem.Decode(bytes.TrimSpace(content))
+			if block == nil {
+				return nil, fmt.Errorf("failed to parse certificate PEM")
+			}
+
+			if block.Type == "CERTIFICATE" {
+				cert, err := x509.ParseCertificate(block.Bytes)
+				if err != nil {
+					return nil, err
+				}
+				certs = append(certs, cert)
+			}
+			if len(rest) == 0 {
+				break
+			}
+			content = rest
+		}
+		return certs, nil
+	default:
+		return nil, fmt.Errorf("unsupported scheme '%s' in location %s", u.Scheme, u.String())
+	}
+}
+
+func getFields(cert *x509.Certificate, now time.Time) map[string]int64 {
+	age := int(now.Sub(cert.NotBefore).Milliseconds())
+	expiry := int(cert.NotAfter.Sub(now).Milliseconds())
+	startdate := cert.NotBefore.Unix()
+	enddate := cert.NotAfter.Unix()
+
+	fields := map[string]int64{
+		"age":       int64(age),
+		"expiry":    int64(expiry),
+		"startdate": startdate,
+		"enddate":   enddate,
+	}
+
+	return fields
+}
+
+func getTags(cert *x509.Certificate, location string) map[string]string {
+	tags := map[string]string{
+		"source":               location,
+		"common_name":          cert.Subject.CommonName,
+		"serial_number":        cert.SerialNumber.Text(16),
+		"signature_algorithm":  cert.SignatureAlgorithm.String(),
+		"public_key_algorithm": cert.PublicKeyAlgorithm.String(),
+	}
+
+	if len(cert.Subject.Organization) > 0 {
+		tags["organization"] = cert.Subject.Organization[0]
+	}
+	if len(cert.Subject.OrganizationalUnit) > 0 {
+		tags["organizational_unit"] = cert.Subject.OrganizationalUnit[0]
+	}
+	if len(cert.Subject.Country) > 0 {
+		tags["country"] = cert.Subject.Country[0]
+	}
+	if len(cert.Subject.Province) > 0 {
+		tags["province"] = cert.Subject.Province[0]
+	}
+	if len(cert.Subject.Locality) > 0 {
+		tags["locality"] = cert.Subject.Locality[0]
+	}
+
+	tags["issuer_common_name"] = cert.Issuer.CommonName
+	tags["issuer_serial_number"] = cert.Issuer.SerialNumber
+
+	san := append(cert.DNSNames, cert.EmailAddresses...)
+	for _, ip := range cert.IPAddresses {
+		san = append(san, ip.String())
+	}
+	for _, uri := range cert.URIs {
+		san = append(san, uri.String())
+	}
+	tags["san"] = strings.Join(san, ",")
+
+	return tags
+}
+
+func (c *X509Cert) collectCertURLs() ([]*url.URL, error) {
+	var urls []*url.URL
+
+	for _, path := range c.globpaths {
+		files := path.Match()
+		if len(files) <= 0 {
+			log.Error("could not find file: {}", path)
+			continue
+		}
+		for _, file := range files {
+			file = "file://" + file
+			u, err := url.Parse(file)
+			if err != nil {
+				return urls, fmt.Errorf("failed to parse cert location - %s", err.Error())
+			}
+			urls = append(urls, u)
+		}
+	}
+
+	return urls, nil
+}
+
+// Gather adds metrics into the accumulator.
+func (c *X509Cert) Gather() (error, []X509CertMetrics) {
+	metrics := make([]X509CertMetrics, 0)
+	now := time.Now()
+	collectedUrls, err := c.collectCertURLs()
+	if err != nil {
+		return err, nil
+	}
+
+	for _, location := range append(c.locations, collectedUrls...) {
+		certs, err := c.getCert(location, time.Duration(c.Timeout))
+		if err != nil {
+			return err, nil
+		}
+
+		for i, cert := range certs {
+			fields := getFields(cert, now)
+			tags := getTags(cert, location.String())
+
+			// The first certificate is the leaf/end-entity certificate which needs DNS
+			// name validation against the URL hostname.
+			opts := x509.VerifyOptions{
+				Intermediates: x509.NewCertPool(),
+				KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageAny},
+			}
+			if i == 0 {
+				opts.DNSName, err = c.serverName(location)
+				if err != nil {
+					return err, nil
+				}
+				for j, cert := range certs {
+					if j != 0 {
+						opts.Intermediates.AddCert(cert)
+					}
+				}
+			}
+			if c.tlsCfg.RootCAs != nil {
+				opts.Roots = c.tlsCfg.RootCAs
+			}
+
+			_, err = cert.Verify(opts)
+			if err == nil {
+				tags["verification"] = "valid"
+				fields["verification_code"] = 0
+			} else {
+				tags["verification"] = "invalid"
+				fields["verification_code"] = 1
+				fields["verification_error"] = reflect.ValueOf(err.Error()).Int()
+			}
+
+			metrics = append(metrics, X509CertMetrics{
+				Tags:   tags,
+				Fields: fields})
+			if c.ExcludeRootCerts {
+				break
+			}
+		}
+	}
+
+	return nil, metrics
+}
+
+func (c *X509Cert) Init() error {
+	err := c.sourcesToURLs()
+	if err != nil {
+		return err
+	}
+
+	tlsCfg, err := c.ClientConfig.TLSConfig()
+	if err != nil {
+		return err
+	}
+	if tlsCfg == nil {
+		tlsCfg = &tls.Config{}
+	}
+
+	if tlsCfg.ServerName != "" && c.ServerName == "" {
+		c.ServerName = tlsCfg.ServerName
+		tlsCfg.ServerName = ""
+	}
+	c.tlsCfg = tlsCfg
+
+	return nil
+}
+
+func run() error {
+	ctx := context.Background()
+	cont, err := createController()
+	if err != nil {
+		panic(fmt.Errorf("error creating controller: %v", err))
+	}
+
+	defer handleErr(cont.Stop(ctx))
+	meter := cont.Meter("x509_certificate_metrics")
+	err = cont.Start(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	cert := &X509Cert{
+		Sources:          nil,
+		Timeout:          0,
+		ServerName:       "",
+		ExcludeRootCerts: false,
+		tlsCfg:           nil,
+		ClientConfig:     _tls.ClientConfig{},
+		locations:        nil,
+		globpaths:        nil,
+	}
+
+	cert.Sources = []string{os.Getenv("CERTIFICATE_URL")}
+	err = cert.Init()
+	if err != nil {
+		return err
+	}
+
+	err = cert.sourcesToURLs()
+	if err != nil {
+		return nil
+	}
+
+	var certMetrics []X509CertMetrics
+	err, certMetrics = cert.Gather()
+	if err != nil {
+		return err
+	}
+
+	log.Debugln("Extracting metrics from certificates")
+	for _, certMetric := range certMetrics {
+		labels := make([]attribute.KeyValue, 0)
+		for key, value := range certMetric.Tags {
+			labels = append(labels, attribute.String(key, value))
+		}
+
+		ageObserverCallback := func(_ context.Context, result metric.Int64ObserverResult) {
+			result.Observe(certMetric.Fields["age"],
+				labels...)
+		}
+
+		expiryObserverCallback := func(_ context.Context, result metric.Int64ObserverResult) {
+			result.Observe(certMetric.Fields["expiry"],
+				labels...)
+		}
+
+		startdateObserverCallback := func(_ context.Context, result metric.Int64ObserverResult) {
+			result.Observe(certMetric.Fields["startdate"],
+				labels...)
+		}
+
+		enddateObserverCallback := func(_ context.Context, result metric.Int64ObserverResult) {
+			result.Observe(certMetric.Fields["enddate"],
+				labels...)
+		}
+
+		_ = metric.Must(meter).NewInt64GaugeObserver(
+			"x509_age",
+			ageObserverCallback,
+			metric.WithDescription("x509 certificate age"),
+		)
+
+		_ = metric.Must(meter).NewInt64GaugeObserver(
+			"x509_expiry",
+			expiryObserverCallback,
+			metric.WithDescription("x509 certificate expiry"),
+		)
+
+		_ = metric.Must(meter).NewInt64GaugeObserver(
+			"x509_start_date",
+			startdateObserverCallback,
+			metric.WithDescription("x509 certificate start date"),
+		)
+
+		_ = metric.Must(meter).NewInt64GaugeObserver(
+			"x509_end_date",
+			enddateObserverCallback,
+			metric.WithDescription("x509 certificate end date"),
+		)
+	}
+
+	/* Time for the controller to send the data before the program ends */
+	time.Sleep(10 * time.Second)
+	log.Debugln("Metrics sent to logzio")
+	return nil
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic(fmt.Errorf("something went wrong: %v", err))
+	}
+}
+
+func createController() (*controller.Controller, error) {
+	config := cortex.Config{
+		Endpoint:      os.Getenv("LOGZIO_LISTENER"),
+		RemoteTimeout: 30 * time.Second,
+		PushInterval:  15 * time.Second,
+		BearerToken:   os.Getenv("LOGZIO_METRICS_TOKEN"),
+	}
+
+	return cortex.InstallNewPipeline(config,
+		controller.WithCollectPeriod(10*time.Second),
+		controller.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+		),
+		),
+	)
+}
